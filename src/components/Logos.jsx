c50fd07cc20e8abd09a9b207d5883e6955bbe3fa
Shreyas D <shreyas24d@gmail.com>
2025-08-02 00:32:29 +0530

    logos loading issue on deployment

--- src/components/Logos.jsx
+++ src/components/Logos.jsx
@@ -9,7 +9,7 @@ const Logos = () => {
             className="flex items-center justify-center px-1 py-1"
           >
             <img
-              src={`./src/assets/logos/${logoNumber}.png`}
+              src={`/logos/${logoNumber}.png`}
               alt={`Logo ${logoNumber}`}
               className="h-16 w-24 sm:h-20 sm:w-28 md:h-24 md:w-32 lg:h-28 lg:w-36 xl:h-32 xl:w-40 2xl:h-36 2xl:w-44 object-contain"
             />
