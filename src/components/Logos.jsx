6b45c368d57d4b1171ee2c705520aadabf33bccc
Shreyas D <shreyas24d@gmail.com>
2025-08-02 00:39:40 +0530

    Adjusted width of the logos component

--- src/components/Logos.jsx
+++ src/components/Logos.jsx
@@ -1,20 +1,22 @@
 const Logos = () => {
   return (
     <div className="w-full bg-white px-4 my-4 sm:my-6 md:my-8 lg:my-12 xl:my-16 2xl:my-0">
-      {/* Responsive flex container */}
-      <div className="flex flex-wrap lg:flex-nowrap justify-center items-center gap-4 sm:gap-6 md:gap-8 lg:gap-6 xl:gap-8 2xl:gap-10">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((logoNumber) => (
-          <div
-            key={logoNumber}
-            className="flex items-center justify-center px-1 py-1"
-          >
-            <img
-              src={`/logos/${logoNumber}.png`}
-              alt={`Logo ${logoNumber}`}
-              className="h-16 w-24 sm:h-20 sm:w-28 md:h-24 md:w-32 lg:h-28 lg:w-36 xl:h-32 xl:w-40 2xl:h-36 2xl:w-44 object-contain"
-            />
-          </div>
-        ))}
+      {/* Responsive flex container with Section 4 width constraint */}
+      <div className="max-w-[120rem] mx-auto">
+        <div className="flex flex-wrap lg:flex-nowrap justify-center items-center gap-4 sm:gap-6 md:gap-8 lg:gap-6 xl:gap-8 2xl:gap-10">
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((logoNumber) => (
+            <div
+              key={logoNumber}
+              className="flex items-center justify-center px-1 py-1"
+            >
+              <img
+                src={`/logos/${logoNumber}.png`}
+                alt={`Logo ${logoNumber}`}
+                className="h-16 w-24 sm:h-20 sm:w-28 md:h-24 md:w-32 lg:h-28 lg:w-36 xl:h-32 xl:w-40 2xl:h-36 2xl:w-44 object-contain"
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
