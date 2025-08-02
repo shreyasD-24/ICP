5f8e2a4cf1bd03d7d2fad49797a5ef4552b55c30
Shaurya Gupta <ShauryaGupta118@gmail.com>
2025-08-02 16:41:39 +0530

    adding anchor on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,15 +7,17 @@ function Navbar() {
   return (
     <nav className="relative flex justify-between items-center px-4 sm:px-8 md:px-12 lg:px-16 xl:px-20 2xl:px-24 3xl:px-32 4xl:px-40 py-4 sm:py-5 md:py-6 lg:py-7 xl:py-8 2xl:py-10 3xl:py-12 4xl:py-14 5xl:py-16">
       <div className="logo">
-        <img
-          src={logo}
-          alt="ICP"
-          className="h-8 xs:h-10 sm:h-12 md:h-14 lg:h-16 xl:h-18 2xl:h-20 3xl:h-22 4xl:h-24 5xl:h-26 w-auto object-contain"
-          style={{
-            filter: "contrast(1.1) brightness(1.05)",
-            imageRendering: "crisp-edges",
-          }}
-        />
+        <a href="#home" className="cursor-pointer">
+          <img
+            src={logo}
+            alt="ICP"
+            className="h-8 xs:h-10 sm:h-12 md:h-14 lg:h-16 xl:h-18 2xl:h-20 3xl:h-22 4xl:h-24 5xl:h-26 w-auto object-contain"
+            style={{
+              filter: "contrast(1.1) brightness(1.05)",
+              imageRendering: "crisp-edges",
+            }}
+          />
+        </a>
       </div>
 
       {/* Desktop Menu */}
